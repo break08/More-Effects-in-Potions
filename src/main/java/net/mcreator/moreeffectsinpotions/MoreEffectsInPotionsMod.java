6ff93777cacad4199a8f8d64718a4602fd1a40fe
NoName123 <dungluong3108@gmail.com>
2026-09-13 21:34:07 +0700

    change

--- src/main/java/net/mcreator/moreeffectsinpotions/MoreEffectsInPotionsMod.java
+++ src/main/java/net/mcreator/moreeffectsinpotions/MoreEffectsInPotionsMod.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package net.mcreator.moreeffectsinpotions;
 
 import org.apache.logging.log4j.Logger;
@@ -86,3 +87,34 @@ public class MoreEffectsInPotionsMod {
 		}
 	}
 }
+=======
+/*
+ *	MCreator note:
+ *
+ *	If you lock base mod element files, you can edit this file and the proxy files
+ *	and they won't get overwritten. If you change your mod package or modid, you
+ *	need to apply these changes to this file MANUALLY.
+ *
+ *
+ *	If you do not lock base mod element files in Workspace settings, this file
+ *	will be REGENERATED on each build.
+ *
+ */
+package net.mcreator.moreeffectsinpotions;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreEffectsInPotionsMod implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "more_effects_in_potions";
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing MoreEffectsInPotionsMod");
+
+	}
+}
+>>>>>>> Stashed changes
